5b41612c8e77d6c68bc3a13b7733be3e916b0d52
Jon Gregory <jongregory75@hotmail.com>
2021-05-20 23:53:43 -0600

    adding repo to github

--- server.js
+++ server.js
@@ -6,21 +6,23 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const dbObj = require("./db/db.json");
-//const noteObj = require("./public/notes.html");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 
+//get for /notes response is notes.html
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+//get for /api/notes response is the db.json object
 app.get("/api/notes", (req, res) => {
   res.json(dbObj);
 });
 
+//post new note to the db.json
 app.post("/api/notes", (req, res) => {
   const dbObj = require("./db/db.json");
   console.log(dbObj);
@@ -38,6 +40,7 @@ app.post("/api/notes", (req, res) => {
   console.log(newNote);
 });
 
+//attempt at a delete function, can't figure out the last part to refresh /notes
 app.delete("/api/notes/:id", (req, res) => {
   const idNumber = req.params.id;
   const dbObj = require("./db/db.json");
@@ -53,9 +56,11 @@ app.delete("/api/notes/:id", (req, res) => {
   //   res.redirect("/api/notes");
 });
 
+//get for wild card pointing to index.html
 app.get("*", (req, res) => {
   const url = req.url === "/" ? "index.html" : req.url;
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+//listener port
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
